6f72dd48b0f914f6266a7e77a05ce8d33a7f0beb
Philippe maladjian <pmaladjian@pcinf04.dom.hilaire>
2017-05-02 10:37:36 +0200

    Modification de retour-ligne.sh pour gérer le fichier en parametre

--- retour-ligne.sh
+++ retour-ligne.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 # lit une liste de ligne dans un fichier puis renvoie
-# à la ligne en ajoutant un ; toute les x 10 lignes
-i=1
-for mail in  `cat /home/xxx/fichier-source.csv`
-do
-        if [ $i -lt 10 ]
-        then
-                echo -n $mail";" >> /home/xxx/result.csv
-                i=$[$i+1]
-        else
-                echo -e "\n"  >> /home/xxx/result.csv                                                                                                                                                                       
-                i=0
-        fi
-done
+# à la ligne en ajoutant un ; toute les x lignes
+
+if [ "i${1}" == "" ]
+then
+	echo "Fichier ou chemin d'accès invalide"
+	echo "{$1}"
+
+	exit 1
+else
+	DIROUT="${HOME}/Scripts/tools"
+	FICOUT=`basename "${1}"`
+	NBLIG=10
+
+	i=1
+	for mail in  `cat "${1}"`
+	do
+        	if [ $i -lt "${NBLIG}" ]
+	        then	
+        	        echo -n $mail";" >> "${DIROUT}"/"new-${FICOUT}"
+                	i=$[$i+1]
+	        else
+        	        echo -e "\n"  >> "${DIROUT}"/"new-${FICOUT}"
+                	i=0
+	        fi
+	done
+fi
+
+exit 0
